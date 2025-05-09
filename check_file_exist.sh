2fd896b29fb65d3c983b3df91c779b5ad8263726
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-09 15:26:07 +0100

    correction

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Ce script vérifie si un fichier donné existe ou non
+echo -n "Entrez le nom du fichier : "
+read fichier
 
-# Demander le nom du fichier à l'utilisateur
-read -p "Entrez le nom du fichier : " fichier
+echo "DEBUG: fichier = '$fichier'"
 
-# Vérifier si le fichier existe
 if [ -f "$fichier" ]; then
     echo "Le fichier '$fichier' existe."
 else
